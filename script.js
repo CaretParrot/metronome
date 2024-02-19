b721bbcef2be6cdae9ee50571a05556cb0ad7c4c
CaretParrot <87083627+CaretParrot@users.noreply.github.com>
2024-02-19 16:30:38 -0600

    Fixed transparent body background script.js

--- script.js
+++ script.js
@@ -121,4 +121,4 @@ function saveTempo() {
     localStorage.setItem("temposSaved", idTree.savedTempos.innerHTML);
 }
 
-document.body.backgroundColor = "transparent !important";
+document.body.style.backgroundColor = "transparent";
