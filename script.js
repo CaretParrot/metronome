c870a81fcd1ccb3af1cb154677545c98cdfdce2b
CaretParrot <87083627+CaretParrot@users.noreply.github.com>
2024-02-18 20:58:48 -0600

    Refactored Code for Effeciency in script.js

--- script.js
+++ script.js
@@ -9,113 +9,112 @@ let audioElements = document.getElementsByClassName("drone");
 idTree.savedTempos.innerHTML = localStorage.getItem("temposSaved");
 
 for (let i = 0; i < audioElements.length; i++) {
-  audioElements[i].volume = 0.15;
+    audioElements[i].volume = 0.15;
 }
 
 function changeBeatCounter() {
-  for (let i = 0; i < beatCounterElements.length; i++) {
-    beatCounterElements[i].style.display = "none";
-  }
+    for (let i = 0; i < beatCounterElements.length; i++) {
+        beatCounterElements[i].style.display = "none";
+    }
 
-  for (let i = 0; i < idTree.beats.value; i++) {
-    beatCounterElements[i].style.display = "flex";
-  }
+    for (let i = 0; i < idTree.beats.value; i++) {
+        beatCounterElements[i].style.display = "flex";
+    }
 }
 
 function refreshCounter() {
-  idTree.accentBeat.pause();
-  idTree.accentBeat.currentTime = 0;
-  idTree.normalBeat.pause();
-  idTree.normalBeat.currentTime = 0;
+    idTree.accentBeat.pause();
+    idTree.accentBeat.currentTime = 0;
+    idTree.normalBeat.pause();
+    idTree.normalBeat.currentTime = 0;
 
-  if (beatCounter === 0 && idTree.beatCounterTable.style.display === "flex") {
-    idTree.accentBeat.play();
-  } else {
-    idTree.normalBeat.play();
-  }
+    if (beatCounter === 0 && idTree.beatCounterTable.style.display === "flex") {
+        idTree.accentBeat.play();
+    } else {
+        idTree.normalBeat.play();
+    }
 
-  for (let i = 0; i < beatCounterElements.length; i++) {
-    beatCounterElements[i].style.backgroundColor = pickedColor[1];
-    beatCounterElements[i].style.color = "Black";
-  }
+    for (let i = 0; i < beatCounterElements.length; i++) {
+        beatCounterElements[i].style.backgroundColor = pickedColor[1];
+        beatCounterElements[i].style.color = "Black";
+    }
 
-  beatCounterElements[beatCounter].style.backgroundColor = pickedColor[2];
-  beatCounterElements[beatCounter].style.color = "Black";
-  beatCounter++;
+    beatCounterElements[beatCounter].style.backgroundColor = pickedColor[2];
+    beatCounterElements[beatCounter].style.color = "Black";
+    beatCounter++;
 
-  if (beatCounter > idTree.beats.value - 1) {
-    beatCounter = 0;
-  }
+    if (beatCounter > idTree.beats.value - 1) {
+        beatCounter = 0;
+    }
 }
 
 changeBeatCounter();
 
 onchange = function (event) {
-  changeBeatCounter();
-  if (idTree.playButton.innerHTML === "Pause") {
-    clearInterval(metronome);
-    for (let i = 0; i < beatCounterElements.length; i++) {
-      beatCounterElements[i].style.backgroundColor = pickedColor[1];
-      beatCounterElements[i].style.color = "Black";
+    changeBeatCounter();
+    if (idTree.playButton.innerHTML === "Pause") {
+        clearInterval(metronome);
+        beatCounter = 0;
+        for (let i = 0; i < beatCounterElements.length; i++) {
+            beatCounterElements[i].style.backgroundColor = pickedColor[1];
+            beatCounterElements[i].style.color = "Black";
+        }
+        refreshCounter();
+        metronome = setInterval(function () {
+            refreshCounter();
+        }, 60000 / idTree.tempo.value);
     }
-    beatCounter = 0;
-    refreshCounter();
-    metronome = setInterval(function () {
-      refreshCounter();
-    }, 60000 / idTree.tempo.value);
-  }
 }
 
 function playMetronome() {
-  if (idTree.playButton.innerHTML === "Play") {
-    refreshCounter();
+    if (idTree.playButton.innerHTML === "Play") {
+        refreshCounter();
+        idTree.playButton.innerHTML = "Pause";
 
-    metronome = setInterval(function () {
-      refreshCounter();
-    }, 60000 / idTree.tempo.value);
+        metronome = setInterval(function () {
+            refreshCounter();
+        }, 60000 / idTree.tempo.value);
 
-    idTree.playButton.innerHTML = "Pause";
+    } else if (idTree.playButton.innerHTML === "Pause") {
+        clearInterval(metronome);
+        idTree.playButton.innerHTML = "Play";
+        beatCounter = 0;
+        for (let i = 0; i < beatCounterElements.length; i++) {
+            beatCounterElements[i].style.backgroundColor = pickedColor[1];
+            beatCounterElements[i].style.color = "Black";
+        }
 
-  } else if (idTree.playButton.innerHTML === "Pause") {
-    clearInterval(metronome);
-    idTree.playButton.innerHTML = "Play";
-    for (let i = 0; i < beatCounterElements.length; i++) {
-      beatCounterElements[i].style.backgroundColor = pickedColor[1];
-      beatCounterElements[i].style.color = "Black"; 
     }
-
-    beatCounter = 0;
-  }
 }
 
 function enableAccent() {
-  if (idTree.beatCounterTable.style.display === "none") {
-    idTree.beatCounterTable.style.display = "flex";
-  } else {
-    idTree.beatCounterTable.style.display = "none";
-  }
+    if (idTree.beatCounterTable.style.display === "none") {
+        idTree.beatCounterTable.style.display = "flex";
+    } else {
+        idTree.beatCounterTable.style.display = "none";
+    }
 }
 
 function playDrone(note) {
-  id(note).play();
+    id(note).play();
 }
 
 function stop() {
-  let drones = classTree.drone;
-  for (let i = 0; i < drones.length; i++) {
-    drones[i].pause();
-    drones[i].currentTime = 0;
-  }
+    let drones = classTree.drone;
+    for (let i = 0; i < drones.length; i++) {
+        drones[i].pause();
+        drones[i].currentTime = 0;
+    }
 }
 
 function deleteTempos() {
-  savedTempos = [];
-  idTree.savedTempos.innerHTML = "";
-  localStorage.setItem("temposSaved", idTree.savedTempos.innerHTML);
+    savedTempos = [];
+    idTree.savedTempos.innerHTML = "";
+    localStorage.setItem("temposSaved", idTree.savedTempos.innerHTML);
 }
 
 function saveTempo() {
-  savedTempos.push(idTree.tempo.value);
-  idTree.savedTempos.innerHTML += `<button style="background-color: transparent;" onclick="document.getElementById('tempo').value = ${id("tempo").value}; document.getElementById('playButton').innerHTML = 'Play'; clearInterval(metronome); beatCounter = 0; playMetronome();">${id("tempo").value}</button>`;
-  localStorage.setItem("temposSaved", idTree.savedTempos.innerHTML);
+    savedTempos.push(idTree.tempo.value);
+    idTree.savedTempos.innerHTML += `<button style="background-color: transparent;" onclick="document.getElementById('tempo').value = ${id("tempo").value}; document.getElementById('playButton').innerHTML = 'Play'; clearInterval(metronome); beatCounter = 0; playMetronome();">${id("tempo").value}</button>`;
+    localStorage.setItem("temposSaved", idTree.savedTempos.innerHTML);
 }
