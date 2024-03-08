138c078ad030e8d5490d469bd6714d090289a74e
CaretParrot <87083627+CaretParrot@users.noreply.github.com>
2024-03-07 21:47:48 -0600

    Moved position of the localStorage call to fix bug with wrong button colors script.js

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ let buttons = document.getElementsByTagName("button");
 let inputs = document.getElementsByTagName("input");
 let drones = document.getElementsByClassName("droneButton");
 
-idTree.savedTempos.innerHTML = localStorage.getItem("temposSaved");
-
 randomColor.paint(50, 60);
 refreshHover();
+
+idTree.savedTempos.innerHTML = localStorage.getItem("temposSaved");
 
 for (let i = 0; i < audioElements.length; i++) {
     audioElements[i].volume = 0.15;
